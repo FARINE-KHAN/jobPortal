74a94657b0f5dfc48a1510efeab4c07b9b5731f9
Muhaz Ahmed <muhazvla313@gmail.com>
2024-01-27 16:51:55 +0530

    added home page

--- client/src/layout/navBar/NavBar.jsx
+++ client/src/layout/navBar/NavBar.jsx
@@ -1,40 +1,92 @@
-import React from "react";
-import {  Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    NavbarMenuToggle,
-    NavbarMenu,
-    NavbarMenuItem,
-    Avatar,
-    Button,
-    Link,} from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+import "./NavBar.css";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Button,
+  Link,
+} from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
+import NavModal from "../../components/nav-modal/NavModal";
+import { newSessionData } from "../../utils/commonFunctions";
+import logo from "../../assets/images/jp-logo.png";
 
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const navigate =useNavigate()
-  const handleNavigation=(path)=>{
-    navigate(path)
-  }
+  const [modal, setModal] = useState(false);
+  const [pageId, setPageId] = useState("");
+  const [mode, setMode] = useState(true);
+
+  const navigate = useNavigate();
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+  const openModal = (page) => {
+    setPageId(page);
+    setModal(true);
+  };
+
   const menuItems = [
-    "Profile",
-    "Dashboard",
-    "My Settings",
-    "Help & Feedback",
-    "Log Out",
+    { label: "About", path: "/about" },
+    { label: "Contact Us", path: "/contact" },
+    { label: "My Settings", path: "/settings" },
+    { label: "Help & Feedback", path: "/help" },
+    { label: "Log Out", path: "/logout" },
   ];
 
+  const changeMode = () => {
+    setMode(!mode);
+    if (mode) {
+      document.documentElement.classList.remove("dark");
+      newSessionData("dark-mode", mode);
+    } else {
+      document.documentElement.classList.add("dark");
+      newSessionData("dark-mode", mode);
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("transparent-bg", !mode);
+    newSessionData("dark-mode", mode);
+  }, [mode]);
+
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
+    <Navbar
+      onMenuOpenChange={setIsMenuOpen}
+      className={mode ? "light-mode" : "dark-mode"}
+    >
+      {modal && (
+        <NavModal
+          setModal={setModal}
+          handleNavigation={handleNavigation}
+          pageId={pageId}
+        />
+      )}
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
         <NavbarBrand>
-          
-          <p onClick={()=>handleNavigation("/")} className="font-bold text-inherit">Job-Portal</p>
+          <img
+            src={logo}
+            alt="logo"
+            onClick={() => handleNavigation("/")}
+            title="Home"
+            className="cursor-pointer"
+          />
+          <p
+            onClick={() => handleNavigation("/")}
+            title="Home"
+            className="font-bold text-inherit cursor-pointer"
+          >
+            Job-Portal
+          </p>
         </NavbarBrand>
       </NavbarContent>
 
@@ -57,26 +109,49 @@ export default function NavBar() {
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Button
+            onClick={() => openModal("login")}
+            color="primary"
+            variant="ghost"
+          >
+            Login
+          </Button>
         </NavbarItem>
-        <NavbarItem>
-          <Button onClick={()=>handleNavigation("/job-seeker/register")} color="primary"  variant="flat">
+        {/* <NavbarItem>
+          <Button
+            onClick={() => openModal("register")}
+            color="primary"
+            variant="flat"
+          >
             Sign Up
+          </Button>
+        </NavbarItem> */}
+        <NavbarItem>
+          <Button color="primary" onClick={changeMode}>
+            {mode ? (
+              <i className="fa-solid fa-sun"></i>
+            ) : (
+              <i className="fa-solid fa-moon"></i>
+            )}
           </Button>
         </NavbarItem>
       </NavbarContent>
       <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
+        {menuItems.map((menuItem, index) => (
+          <NavbarMenuItem key={`${menuItem.label}-${index}`}>
             <Link
               color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
               }
               className="w-full"
-              href="#"
+              href={menuItem.path}
               size="lg"
             >
-              {item}
+              {menuItem.label}
             </Link>
           </NavbarMenuItem>
         ))}
